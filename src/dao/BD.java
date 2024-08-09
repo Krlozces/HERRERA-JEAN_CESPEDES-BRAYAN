b7dcc4f61743ba27faffd1c175fbf4d15c3a6874
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-08-08 22:04:00 -0500

    Read file

--- src/dao/BD.java
+++ src/dao/BD.java
@@ -1,8 +1,22 @@
 package dao;
+import org.h2.tools.RunScript;
+
+import java.io.FileReader;
 import java.sql.*;
 import java.sql.DriverManager;
 
 public class BD {
+    public static void ejecutarScript() {
+        Connection connection = null;
+        try {
+            connection = getConnection();
+
+            RunScript.execute(connection, new FileReader("src/Script.sql"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static Connection getConnection() throws Exception{
         Class.forName("org.h2.Driver");
         return DriverManager.getConnection("jdbc:h2:mem:runnerz", "sa", "");
